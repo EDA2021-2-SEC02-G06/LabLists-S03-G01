6dd739a6e2e03513d852582238abdf191ae4ef3e
Nicolas <ny.rivera@uniandes.edu.co>
2021-08-31 21:18:45 -0500

    Paso 5

--- App/model.py
+++ App/model.py
@@ -54,7 +54,7 @@ def newCatalog():
     catalog['books'] = lt.newList()
     catalog['authors'] = lt.newList('ARRAY_LIST',
                                     cmpfunction=compareauthors)
-    catalog['tags'] = lt.newList('ARRAY_LIST',
+    catalog['tags'] = lt.newList('SINGLE_LINKED',
                                  cmpfunction=comparetagnames)
     catalog['book_tags'] = lt.newList('ARRAY_LIST')
 
